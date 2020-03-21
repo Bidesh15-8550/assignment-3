7efeafc8ad584c7c78d97c7a90c3cab3731b6f37
Bidesh15-8550 <bideshbiswas@ymail.com>
2020-03-21 18:06:48 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="shortcut icon" href="images/calendar.png" type="image/x-icon">
+    <title>Assignment 3</title>
+</head>
+<body>
+    <div class="box">
+        <br>
+        <div class="block" style="height:20px;width:98%; background-color:#8B0000 ;margin:auto">
+
+        </div>
+        <div class="title">
+            <h1>COMP207 - Register here for a practical slot</h1>
+            <h3>Register only if you know what you are doing.</h3>
+           
+            <ul>
+            <li>Please enter <b>all</b> information and select your desired day. Please enter a correct 'SID' number!</li>
+            <li>Please check the number of available seats before submitting.</li>
+            <li>Register only to on slot.</li>
+            <li>Any problems? Write a message to <a href="" style=" text-decoration: none;  color: #9E2929;">weberb@cse.liv.ac.uk</a></li>
+            </ul>
+        </div>
+        <div class="block" style="height:20px;width:98%; background-color:#8B0000 ;margin:auto">
+
+        </div>
+        <br>
+    
+    </div>
+</body>
+</html>
